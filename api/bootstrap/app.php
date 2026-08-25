b61461c7300b7823f59b5d79418acfc6aea9e90c
al26 <amriluthfi26@gmail.com>
2026-08-25 14:09:22 +0700

    feat: enable resend mail driver

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -17,7 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         AuthServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->preventRequestForgery(except: [
+            'resend/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
